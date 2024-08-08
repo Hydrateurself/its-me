477f84eddee3f805ed972c07f2e222d6cf34deda
Hydrateurself <bensn@posteo.de>
2024-08-08 13:34:36 +0200

    margin

--- src/components/section-six.tsx
+++ src/components/section-six.tsx
@@ -10,7 +10,7 @@ export const SectionSix = () => {
         id="section6"
         className="w-full h-auto bg-green-200 bg-gradient-to-b from-green-200 via-green-100 to-neutral-50"
       >
-        <h2 className="font-ultra text-4xl lg:text-6xl pl-4 pr-4 pt-4 md:p-6">
+        <h2 className="font-ultra text-4xl lg:text-6xl pl-4 pr-4 pt-4 md:p-6 md:ml-10">
           You reached the end of the Page
         </h2>
         <div className="flex flex-col md:flex-row justify-center w-full p-4 md:p-6 gap-6">
